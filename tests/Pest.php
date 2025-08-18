44f5215ed0e4405d8e409e3f99ef30d08a3be2d0
maximemockelyn <maximemockelyn8@gmail.com>
2025-08-18 02:24:50 +0200

    test: add unit tests for app install command and refresh database
    - Add test case for handle command with license validation
    - Include RefreshDatabase trait in Pest test setup
    - Update .gitignore to exclude coverage directory

--- tests/Pest.php
+++ tests/Pest.php
@@ -16,6 +16,7 @@ pest()->extend(Tests\TestCase::class)
     ->in('Feature');
 
 pest()->extend(Tests\TestCase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Unit');
 
 /*
